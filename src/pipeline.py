76d47ce54423a5b4fa75a726887e9e14db715de5
gordonbuck <grdnbuck@googlemail.com>
2020-03-30 00:50:57 +0100

    u

--- src/pipeline.py
+++ src/pipeline.py
@@ -19,26 +19,15 @@ if __name__ == '__main__':
     device = torch.device(f'cuda:{args.cuda}' if args.cuda != -1 else 'cpu')
     model = HICE(args.n_head, w2v.vector_size, 2 * args.ctx_len, args.n_layer, source_corpus.dictionary.idx2vec,
                  use_morph=args.use_morph)
-
-    if not (args.hice or args.maml or args.leap):
-        print("Loading Chimera corpus")
-        target_corpus = Corpus(Path(args.chimera_dir), w2v, w2v_lbound=args.w2v_lbound, w2v_ubound=args.w2v_ubound,
-                               corpus_lbound=args.corpus_lbound, ctx_len=args.ctx_len,
-                               dictionary=source_corpus.dictionary, is_wikitext=False)
-        model.update_embedding(target_corpus.dictionary.idx2vec)
+    print("Loading Chimera corpus")
+    target_corpus = Corpus(Path(args.chimera_dir), w2v, w2v_lbound=args.w2v_lbound, w2v_ubound=args.w2v_ubound,
+                           corpus_lbound=args.corpus_lbound, ctx_len=args.ctx_len,
+                           dictionary=source_corpus.dictionary, is_wikitext=False)
+    model.update_embedding(target_corpus.dictionary.idx2vec)
 
     if args.hice:
         print("Training")
         train(model, source_corpus, char2idx, args, device)
-
-    if args.maml or args.leap:
-        print("Loading Chimera corpus")
-        target_corpus = Corpus(Path(args.chimera_dir), w2v, w2v_lbound=args.w2v_lbound, w2v_ubound=args.w2v_ubound,
-                               corpus_lbound=args.corpus_lbound, ctx_len=args.ctx_len,
-                               dictionary=source_corpus.dictionary, is_wikitext=False)
-        model.load_state_dict(torch.load(os.path.join(args.save_dir, 'model.pt')))
-        model.update_embedding(target_corpus.dictionary.idx2vec)
-        torch.save(model.state_dict(), os.path.join(args.save_dir, 'model.pt'))
 
     if args.maml:
         print("MAML adaptation")
