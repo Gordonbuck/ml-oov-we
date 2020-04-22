8a3b8f8ae3acdd45ff6555ca86cb484294a5b269
gordonbuck <grdnbuck@googlemail.com>
2020-04-22 15:23:09 +0100

    bug

--- src/jnlpba.py
+++ src/jnlpba.py
@@ -39,7 +39,7 @@ def preprocess_jnlpba(jnlpba_dir, corpus):
                 s = train
                 train_size += 1
 
-            for w in (jnlpba_dir / 'train/Genia4ERtask1.iob2').open().readlines()[:-1]:
+            for w in (jnlpba_dir / 'train/Genia4ERtask1.iob2').open().readlines():
                 w = w.strip()
                 if not (w == '' or len(w.split()) == 2):
                     continue
@@ -56,13 +56,14 @@ def preprocess_jnlpba(jnlpba_dir, corpus):
                     else:
                         s = train
                         train_size += 1
+    print(w)
 
     print("Writing JNLPBA test files")
     with (jnlpba_dir / "test.txt").open(mode='w+') as f_test:
         sent = []
         contains_oov = False
 
-        for w in (jnlpba_dir / 'test/Genia4EReval1.iob2').open().readlines()[:-1]:
+        for w in (jnlpba_dir / 'test/Genia4EReval1.iob2').open().readlines():
             w = w.strip()
             if not (w == '' or len(w.split()) == 2):
                 continue
@@ -79,5 +80,6 @@ def preprocess_jnlpba(jnlpba_dir, corpus):
                 contains_oov = True
 
             sent += w
+    print(w)
 
     print(f'JNLPBA train size: {train_size} dev size: {dev_size} test size: {test_size}')
